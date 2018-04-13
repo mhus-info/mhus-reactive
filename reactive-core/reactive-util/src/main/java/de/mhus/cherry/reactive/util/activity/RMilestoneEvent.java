f335574b28ac78fb4fc91edb22c6aedbe2630e72
Mike Hummel <>
2018-04-13 21:31:20 +0200

    add milestone event

---
+++ reactive-core/reactive-util/src/main/java/de/mhus/cherry/reactive/util/activity/RMilestoneEvent.java
@@ -0,0 +1,19 @@
+package de.mhus.cherry.reactive.util.activity;
+
+import de.mhus.cherry.reactive.util.bpmn2.RPool;
+
+public abstract class RMilestoneEvent <P extends RPool<?>> extends REvent<P> {
+
+	@Override
+	public void doExecute() throws Exception {
+		getContext().getPCase().setMilestone(getMilestone());
+	}
+
+	/**
+	 * Return the new milestone.
+	 * 
+	 * @return The milestone
+	 */
+	protected abstract String getMilestone();
+
+}
