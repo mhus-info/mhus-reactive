2c0b3fe14b55f9e499e2c32e28c46bd21b6772b8
Mike Hummel <>
2019-04-01 23:30:56 +0200

    version 1.6.0

--- reactive-core/reactive-test/src/main/java/de/mhus/cherry/reactive/examples/simple1/_S1Pool.java
+++ reactive-core/reactive-test/src/main/java/de/mhus/cherry/reactive/examples/simple1/_S1Pool.java
@@ -5,45 +5,44 @@ import de.mhus.lib.basics.consts.ConstBase;
 
 /**
  * File created by mhu const generator. Changes will be overwritten.
- * 28.2.2019
  **/
 
 public class _S1Pool extends ConstBase {
 
-  public static final Identifier CLASS_EXTENDS = new Identifier(de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"de.mhus.cherry.reactive.util.bpmn2.RPool");
-  public static final Identifier CLASS_NAME = new Identifier(de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"de.mhus.cherry.reactive.examples.simple1.S1Pool");
-  public static final Identifier CLASS_PATH = new Identifier(de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"de.mhus.cherry.reactive.examples.simple1.S1Pool");
-  public static final Identifier METHOD_AFTER_EXECUTE = new Identifier(de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"afterExecute");
-  public static final Identifier METHOD_BEFORE_EXECUTE = new Identifier(de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"beforeExecute");
-  public static final Identifier METHOD_CLOSE_CASE = new Identifier(de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"closeCase");
-  public static final Identifier METHOD_CREATE_INDEX_VALUES = new Identifier(de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"createIndexValues");
-  public static final Identifier METHOD_EXPORT_PARAMTERS = new Identifier(de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"exportParamters");
-  public static final Identifier METHOD_GET_POJO_MODEL = new Identifier(de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"getPojoModel");
-  public static final Identifier METHOD_GET_TEXT1 = new Identifier(de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"getText1");
-  public static final Identifier METHOD_GET_TEXT2 = new Identifier(de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"getText2");
-  public static final Identifier METHOD_IMPORT_PARAMETERS = new Identifier(de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"importParameters");
-  public static final Identifier METHOD_INITIALIZE_CASE = new Identifier(de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"initializeCase");
-  public static final Identifier METHOD_LOG = new Identifier(de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"log");
-  public static final Identifier METHOD_SET_CONTEXT = new Identifier(de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"setContext");
-  public static final Identifier METHOD_SET_TEXT1 = new Identifier(de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"setText1");
-  public static final Identifier METHOD_SET_TEXT2 = new Identifier(de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"setText2");
-  public static final Identifier PROJECT_ARTIFACT = new Identifier(de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"reactive-test");
-  public static final Identifier PROJECT_DESCRIPTION = new Identifier(de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"BPM Implementation base on a java class model. It allows to create more complex and refactorable models.");
-  public static final Identifier PROJECT_GROUP = new Identifier(de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"de.mhus.cherry.reactive");
-  public static final Identifier PROJECT_VERSION = new Identifier(de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"1.6.0-SNAPSHOT");
-  public static final Identifier _AFTER_EXECUTE = new Identifier(de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"afterExecute");
-  public static final Identifier _BEFORE_EXECUTE = new Identifier(de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"beforeExecute");
-  public static final Identifier _CHECK_INPUT_PARAMETERS = new Identifier(de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"checkInputParameters");
-  public static final Identifier _CLOSE_CASE = new Identifier(de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"closeCase");
-  public static final Identifier _CONTEXT = new Identifier(de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"Context");
-  public static final Identifier _CREATE_INDEX_VALUES = new Identifier(de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"createIndexValues");
-  public static final Identifier _EXPORT_PARAMTERS = new Identifier(de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"exportParamters");
-  public static final Identifier _IMPORT_PARAMETERS = new Identifier(de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"importParameters");
-  public static final Identifier _INITIALIZE_CASE = new Identifier(de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"initializeCase");
-  public static final Identifier _LOG = new Identifier(de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"log");
-  public static final Identifier _POJO_MODEL = new Identifier(de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"PojoModel");
-  public static final Identifier _REGISTER_NATIVES = new Identifier(de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"registerNatives");
-  public static final Identifier _TEXT1 = new Identifier(de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"Text1");
-  public static final Identifier _TEXT2 = new Identifier(de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"Text2");
+  public static final Identifier CLASS_EXTENDS = new Identifier(Identifier.TYPE.CLASS, de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"de.mhus.cherry.reactive.util.bpmn2.RPool");
+  public static final Identifier CLASS_NAME = new Identifier(Identifier.TYPE.CLASS, de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"de.mhus.cherry.reactive.examples.simple1.S1Pool");
+  public static final Identifier CLASS_PATH = new Identifier(Identifier.TYPE.CLASS, de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"de.mhus.cherry.reactive.examples.simple1.S1Pool");
+  public static final Identifier METHOD_AFTER_EXECUTE = new Identifier(Identifier.TYPE.ACTION, de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"afterExecute");
+  public static final Identifier METHOD_BEFORE_EXECUTE = new Identifier(Identifier.TYPE.ACTION, de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"beforeExecute");
+  public static final Identifier METHOD_CLOSE_CASE = new Identifier(Identifier.TYPE.ACTION, de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"closeCase");
+  public static final Identifier METHOD_CREATE_INDEX_VALUES = new Identifier(Identifier.TYPE.ACTION, de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"createIndexValues");
+  public static final Identifier METHOD_EXPORT_PARAMTERS = new Identifier(Identifier.TYPE.ACTION, de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"exportParamters");
+  public static final Identifier METHOD_GET_POJO_MODEL = new Identifier(Identifier.TYPE.GETTER, de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"getPojoModel");
+  public static final Identifier METHOD_GET_TEXT1 = new Identifier(Identifier.TYPE.GETTER, de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"getText1");
+  public static final Identifier METHOD_GET_TEXT2 = new Identifier(Identifier.TYPE.GETTER, de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"getText2");
+  public static final Identifier METHOD_IMPORT_PARAMETERS = new Identifier(Identifier.TYPE.ACTION, de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"importParameters");
+  public static final Identifier METHOD_INITIALIZE_CASE = new Identifier(Identifier.TYPE.ACTION, de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"initializeCase");
+  public static final Identifier METHOD_LOG = new Identifier(Identifier.TYPE.ACTION, de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"log");
+  public static final Identifier METHOD_SET_CONTEXT = new Identifier(Identifier.TYPE.SETTER, de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"setContext");
+  public static final Identifier METHOD_SET_TEXT1 = new Identifier(Identifier.TYPE.SETTER, de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"setText1");
+  public static final Identifier METHOD_SET_TEXT2 = new Identifier(Identifier.TYPE.SETTER, de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"setText2");
+  public static final Identifier PROJECT_ARTIFACT = new Identifier(Identifier.TYPE.MAVEN, de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"reactive-test");
+  public static final Identifier PROJECT_DESCRIPTION = new Identifier(Identifier.TYPE.MAVEN, de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"BPM Implementation base on a java class model. It allows to create more complex and refactorable models.");
+  public static final Identifier PROJECT_GROUP = new Identifier(Identifier.TYPE.MAVEN, de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"de.mhus.cherry.reactive");
+  public static final Identifier PROJECT_VERSION = new Identifier(Identifier.TYPE.MAVEN, de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"1.6.0");
+  public static final Identifier _AFTER_EXECUTE = new Identifier(Identifier.TYPE.ACTION, de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"afterExecute");
+  public static final Identifier _BEFORE_EXECUTE = new Identifier(Identifier.TYPE.ACTION, de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"beforeExecute");
+  public static final Identifier _CHECK_INPUT_PARAMETERS = new Identifier(Identifier.TYPE.ACTION, de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"checkInputParameters");
+  public static final Identifier _CLOSE_CASE = new Identifier(Identifier.TYPE.ACTION, de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"closeCase");
+  public static final Identifier _CONTEXT = new Identifier(Identifier.TYPE.SETTER, de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"Context");
+  public static final Identifier _CREATE_INDEX_VALUES = new Identifier(Identifier.TYPE.ACTION, de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"createIndexValues");
+  public static final Identifier _EXPORT_PARAMTERS = new Identifier(Identifier.TYPE.ACTION, de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"exportParamters");
+  public static final Identifier _IMPORT_PARAMETERS = new Identifier(Identifier.TYPE.ACTION, de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"importParameters");
+  public static final Identifier _INITIALIZE_CASE = new Identifier(Identifier.TYPE.ACTION, de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"initializeCase");
+  public static final Identifier _LOG = new Identifier(Identifier.TYPE.ACTION, de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"log");
+  public static final Identifier _POJO_MODEL = new Identifier(Identifier.TYPE.GETTER, de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"PojoModel");
+  public static final Identifier _REGISTER_NATIVES = new Identifier(Identifier.TYPE.ACTION, de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"registerNatives");
+  public static final Identifier _TEXT1 = new Identifier(Identifier.TYPE.GETTER, de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"Text1");
+  public static final Identifier _TEXT2 = new Identifier(Identifier.TYPE.GETTER, de.mhus.cherry.reactive.examples.simple1.S1Pool.class,"Text2");
 
 }
