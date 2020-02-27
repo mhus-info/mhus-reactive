bf4aa1a0f3ea6b02f01d7c30718b27e12553143f
Mike Hummel <mike.hummel@billingsolutions.de>
2020-02-27 15:21:35 +0100

    prepare docker image

--- reactive-docker/reactive-playground-docker/create.sh
+++ reactive-docker/reactive-playground-docker/create.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
 
-docker build . -t reactive-playground-docker:latest
+VERSION=6.3.0
+REPOSITORY=mhus/reactive-playground
 
+if [  ! -f Dockerfile ]; then
+  echo "not a docker configuration"
+  return 1
+fi
+
+
+if [ "$1" = "clean" ]; then
+    shift
+    docker rmi $REPOSITORY:$VERSION
+    docker build --no-cache -t $REPOSITORY:$VERSION .
+else
+    docker build -t $REPOSITORY:$VERSION .
+fi
+
+if [ "$1" = "push" ]; then
+    docker push "$REPOSITORY:$VERSION"
+fi
