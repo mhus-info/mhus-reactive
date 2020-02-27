366ef6190fe732cbd10c24c6490df620e50d4ce0
Mike Hummel <mike.hummel@billingsolutions.de>
2020-02-27 20:58:30 +0100

    reorg cluster api

--- reactive-core/reactive-osgi/src/main/java/de/mhus/cherry/reactive/osgi/impl/ClusterLockProvider.java
+++ reactive-core/reactive-osgi/src/main/java/de/mhus/cherry/reactive/osgi/impl/ClusterLockProvider.java
@@ -33,16 +33,12 @@ public class ClusterLockProvider extends MLog implements CaseLockProvider {
 
     @Override
     public Lock acquireCleanupMaster() {
-        if (M.l(ClusterApi.class).isMaster("reactive_cleanup_" + name))
-            return new MasterLock("reactive_cleanup_" + name);
-        return null;
+        return M.l(ClusterApi.class).getLock("reactive_cleanup_" + name).lock();
     }
 
     @Override
     public Lock acquirePrepareMaster() {
-        if (M.l(ClusterApi.class).isMaster("reactive_prepare_" + name))
-            return new MasterLock("reactive_prepare_" + name);
-        return null;
+        return M.l(ClusterApi.class).getLock("reactive_prepare_" + name).lock();
     }
 
     @Override
@@ -70,69 +66,5 @@ public class ClusterLockProvider extends MLog implements CaseLockProvider {
     public boolean isReady() {
         return M.l(ClusterApi.class).isReady();
     }
-    
-    private class MasterLock implements Lock {
 
-        private String name;
-
-        public MasterLock(String name) {
-            this.name = name;
-        }
-
-        @Override
-        public Lock lock() {
-            return this;
-        }
-
-        @Override
-        public boolean lock(long timeout) {
-            return true;
-        }
-
-        @Override
-        public boolean unlock() {
-            return true;
-        }
-
-        @Override
-        public void unlockHard() {
-            
-        }
-
-        @Override
-        public boolean isLocked() {
-            return true;
-        }
-
-        @Override
-        public String getName() {
-            return name;
-        }
-
-        @Override
-        public String getOwner() {
-            return null;
-        }
-
-        @Override
-        public long getLockTime() {
-            return 0;
-        }
-
-        @Override
-        public boolean refresh() {
-            return true;
-        }
-
-        @Override
-        public long getCnt() {
-            return 0;
-        }
-
-        @Override
-        public String getStartStackTrace() {
-            return null;
-        }
-        
-    }
 }
