21eadc441b4bf05611a7e301c0e9a6538ad45e46
Mike Hummel <mike.hummel@billingsolutions.de>
2020-06-17 17:33:32 +0200

    fix assemly

--- assembly/create.sh
+++ assembly/create.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
 
-VERSION=7.1.0
+VERSION=7.2.0
+REPOSITORY=mhus/reactive-playground
 
 mvn install -P assembly || exit 1
 
 cd reactive-playground-docker
-./create.sh $@ || exit 1
+
+if [  ! -f Dockerfile ]; then
+  echo "not a docker configuration"
+  return 1
+fi
+
+if [ "$1" = "clean" ]; then
+    shift
+    docker rmi $REPOSITORY:$VERSION
+    docker build --no-cache -t $REPOSITORY:$VERSION .
+else
+    docker build -t $REPOSITORY:$VERSION .
+fi
+
+if [ "$1" = "push" ]; then
+    docker push "$REPOSITORY:$VERSION"
+fi
+
 cd ..
 
 if [ "$1" = "test" ]; then
