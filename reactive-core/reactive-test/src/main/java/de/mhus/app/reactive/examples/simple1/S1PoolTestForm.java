dc4b00854963d4964b045b6ae2914c288111ab7c
Mike Hummel <mike.hummel@billingsolutions.de>
2021-07-01 17:51:59 +0200

    Update S1PoolTestForm.java

--- reactive-core/reactive-test/src/main/java/de/mhus/app/reactive/examples/simple1/S1PoolTestForm.java
+++ reactive-core/reactive-test/src/main/java/de/mhus/app/reactive/examples/simple1/S1PoolTestForm.java
@@ -1,10 +1,10 @@
 package de.mhus.app.reactive.examples.simple1;
 
-import de.mhus.lib.core.definition.DefAttribute;
 import de.mhus.lib.core.definition.DefRoot;
 import de.mhus.lib.form.ActionHandler;
 import de.mhus.lib.form.FormControl;
 import de.mhus.lib.form.IFormInformation;
+import de.mhus.lib.form.definition.FaShowInformationPanel;
 import de.mhus.lib.form.definition.FmText;
 
 public class S1PoolTestForm implements IFormInformation {
@@ -12,7 +12,7 @@ public class S1PoolTestForm implements IFormInformation {
     @Override
     public DefRoot getForm() {
         return new DefRoot( 
-            new DefAttribute("showInformation", true),
+            new FaShowInformationPanel(),
             new FmText("name", "Name", "Insert the name"),
             new FmText("desc", "Description", "Insert description")
         );
