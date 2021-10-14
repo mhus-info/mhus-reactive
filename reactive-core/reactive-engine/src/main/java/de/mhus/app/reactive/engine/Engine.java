99bb7a4df7767820a94668211565796601954231
Mike Hummel <mike.hummel@billingsolutions.de>
2021-10-14 12:34:03 +0200

    Update Engine.java

--- reactive-core/reactive-engine/src/main/java/de/mhus/app/reactive/engine/Engine.java
+++ reactive-core/reactive-engine/src/main/java/de/mhus/app/reactive/engine/Engine.java
@@ -222,7 +222,7 @@ public class Engine extends MLog implements EEngine, InternalEngine {
                 PCaseLock lockx =
                         getCaseLockOrNull(
                                 nodeInfo,
-                                "run:node:" + nodeInfo.getCanonicalName(),
+                                "node:" + nodeInfo.getCanonicalName(),
                                 "id",
                                 nodeInfo.getId());
                 if (lockx == null || !(lockx instanceof EngineCaseLock)) {
